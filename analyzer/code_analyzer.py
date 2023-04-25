95346845b8f086f7f93241cb81cbca4e376da41e
pollyblo <ugzcollet@gmail.com>
2023-04-25 16:40:36 +0200

    fix: refacto with  LineTooLongError

--- analyzer/code_analyzer.py
+++ analyzer/code_analyzer.py
@@ -1,10 +1,22 @@
 # write your code here
-file_path = input()
 
-with open (file_path, 'r') as file:
-    line_counter = 0
-    for line in file:
-        if len(line.strip()) > 79:
-            print(f'Line {line_counter + 1}: S001 Too long')
-        line_counter += 1
+class LineTooLongError(Exception):
+    def __init__(self, line_index):
+        self.line_index = line_index
 
+    def __str__(self):
+        return f'Line {self.line_index}: S001 Too long'
+
+
+def analyze_file():
+    with open(input(), 'r') as file:
+        for num, line in enumerate(file, start=1):
+            try:
+                if len(line.strip()) > 79:
+                    raise LineTooLongError(num)
+            except LineTooLongError as e:
+                print(e)
+
+
+if __name__ == '__main__':
+    analyze_file()
